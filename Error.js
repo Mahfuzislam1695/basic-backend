46653b46b0f3f699b23bfdf847641fa2acc12574
MAHFUZ ISLAM <77238922+Mahfuzislam1695@users.noreply.github.com>
2022-11-09 23:07:51 +0630

    Error

---
+++ Error.js
@@ -0,0 +1,21 @@
+async function getData(){
+    try {
+        //undefined.find();
+        const emailError = new Error("Email is already exists")
+        throw emailError;
+    } catch (error) {
+        errorHandler(error);
+    }
+}
+
+getData()
+
+function errorHandler(error) {
+    const {name, message, stack} = error;
+    console.log(name, message);
+
+    // logger.error({name, message, stack})
+    // console.log('internal server error');
+}
+
+console.log("Done");
